1f1542b71582bab55ba8930c614f5ac4f2a1d74c
Gateway58 <znerol@mail.de>
2022-04-15 20:10:06 +0200

    Added callability to the main function

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,51 @@
+use regress::Regex;
 use std::{
     fs::File,
-    io::{BufReader, Read},
+    io::Read,
+    io::{BufReader, Write},
 };
-pub mod regexx;
-use format as f;
-use print as p;
-use regress::Regex;
 fn main() {
-    // regexx::run();
-    let filter_classes = Regex::with_flags(
-        r#".+?(?=<td class="list inline_header" colspan="5"|<\/table><p>)"#,
+    let path_to_create = "../output/";
+    let file = File::open("../subst_001.htm").unwrap();
+    let mut reader = BufReader::new(file);
+    let mut buffer = Vec::new();
+    reader.read_to_end(&mut buffer).unwrap();
+    let file_input = convert_to_utf8_string(&buffer);
+
+    // Regex
+    let head_html_regex =
+        Regex::with_flags(r#".+?(?=<td class="list inline_header" colspan="5")"#, "s").unwrap();
+
+    let class_regex = Regex::with_flags(
+        r#"(<td class="list inline_header" colspan="5")(?:.*?(?=\1|<\/table>))"#,
         "gs",
     )
     .unwrap();
-    let file = File::open("../stundenplan.txt").unwrap();
-    let mut reader = BufReader::new(file);
-    let mut buffer = Vec::new();
 
-    // Read file into vector.
-    reader.read_to_end(&mut buffer).unwrap();
+    let mut head_html = file_input[head_html_regex.find(&file_input).unwrap().range()].to_string();
+    let foot_html = r#"</table><p><font size="3" face="Arial">Stpl_21_22_finis</font></center><p><center><font face="Arial" size="2"><a href="http://www.untis.at" target="_blank" >Untis Stundenplan Software</a></font></center></body></html>"#;
+    // Don't reload
+    head_html = head_html.replace(
+        r#"<meta http-equiv="refresh" content="4; URL=subst_001.htm">"#,
+        "",
+    );
+    // Use utf8 instead of some java instant legacy encoding
+    head_html = head_html.replace(r"iso-8859-1", "utf8");
 
-    // Read.
-    let file_input = convert_to_string(&buffer);
-    let mut prerequisite = String::new();
-    for (enumerator, class) in filter_classes.find_iter(&file_input).enumerate() {
-        if enumerator == 0 {
-            prerequisite = file_input[class.range()].to_string();
-            continue;
-        }
-        let current_class_html = &file_input[class.range()].to_string();
-        let current_class = current_class_html.split_once(">").unwrap().1.to_string()
-            + &current_class_html.split_once("<").unwrap().0.to_string();
+    // Get the html of all classes
+    for regex_match in class_regex.find_iter(&file_input) {
+        let class_html = &file_input[regex_match.range()].to_string();
+        let mut class = class_html.split_once(">").unwrap().1.to_string();
+        // 3 chars should be enough
+        class = class[0..3].trim().to_string();
+        //Combines the head html with the class-html
+        let mut class_file = File::create(format!("{}{}.html", path_to_create, &class)).unwrap();
+        class_file
+            .write(format!("{}{}{}", head_html, class_html, foot_html).as_bytes())
+            .unwrap();
     }
 }
-fn convert_to_string(vec_of_bytes: &Vec<u8>) -> String {
+fn convert_to_utf8_string(vec_of_bytes: &Vec<u8>) -> String {
+    // why can't it just be in utf8?
     vec_of_bytes.into_iter().map(|&item| item as char).collect()
 }
-// let regex = Regex::with_flags(
-//     r#"(<td class="list inline_header" colspan="5")(?:.+?(?=\1|<\/table>))"#,
-//     "gs",
-// )
-// .unwrap();
