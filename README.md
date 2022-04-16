8e60f70a135a60996dca1ec2d9e9cabf437d8afd
Gateway58 <znerol@mail.de>
2022-04-16 03:19:17 +0200

    Added cli features

---
+++ README.md
@@ -0,0 +1,8 @@
+#class-segregation
+TODO: 
+OOP (ofc)
+Windows support (pathetic)
+Clean code up
+write a meaningful README.md
+Don't make it look like it was written by a noob
+use arch btw
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use regress::Regex;
 use std::{
+    env::args,
     fs::File,
     io::Read,
     io::{BufReader, Write},
 };
 fn main() {
-    let path_to_create = "../output/";
-    let file = File::open("../subst_001.htm").unwrap();
+    let path = args().nth(1);
+    if let None = path {
+        print!("Usage: command [FILE] [OUTPUT]\n{}", "Reads in a file, splits the classes up and writes single utf8 encoded html files with the information for the class only.\nBy default [OUTPUT] will be your current directory");
+        return;
+    }
+    let path = path.unwrap();
+    let mut output_dir = args().nth(2).unwrap_or(String::new());
+    if &output_dir.as_bytes()[output_dir.len() - 1] != &b'/' {
+        output_dir += "/";
+    }
+    let file =
+        File::open(&path).expect(format!("No such file with path `{}` found", &path).as_str());
     let mut reader = BufReader::new(file);
     let mut buffer = Vec::new();
     reader.read_to_end(&mut buffer).unwrap();
@@ -23,7 +34,10 @@ fn main() {
     .unwrap();
 
     let mut head_html = file_input[head_html_regex.find(&file_input).unwrap().range()].to_string();
-    let foot_html = r#"</table><p><font size="3" face="Arial">Stpl_21_22_finis</font></center><p><center><font face="Arial" size="2"><a href="http://www.untis.at" target="_blank" >Untis Stundenplan Software</a></font></center></body></html>"#;
+    let foot_html = r#"</table><p><font size="3"
+        face="Arial">Stpl_21_22_finis</font></center><p><center><font face="Arial" size="2"><a
+        href="http://www.untis.at" target="_blank" >Untis Stundenplan
+        Software</a></font></center></body></html>"#;
     // Don't reload
     head_html = head_html.replace(
         r#"<meta http-equiv="refresh" content="4; URL=subst_001.htm">"#,
@@ -39,7 +53,13 @@ fn main() {
         // 3 chars should be enough
         class = class[0..3].trim().to_string();
         //Combines the head html with the class-html
-        let mut class_file = File::create(format!("{}{}.html", path_to_create, &class)).unwrap();
+        let mut class_file = File::create(format!("{}{}.html", output_dir, &class)).expect(
+            format!(
+                "Couldn't create file `{}`",
+                format!("{}{}.html", output_dir, &class)
+            )
+            .as_str(),
+        );
         class_file
             .write(format!("{}{}{}", head_html, class_html, foot_html).as_bytes())
             .unwrap();
